3680f759492f7c99a9e566ee15ebef9deca003ee
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-26 11:33:01 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,7 @@ for (i=0;i<=100;i++){
   // Pour i=0, i incremente 1 jusqu'à 100
   if (i%35 == 0){
     // si i est un multiple de 5 et 7
-  console.log("fizzbuz")
+  console.log("fizzbuzz")
   // Ecrire fizzbuzz
   } else if (i%7 == 0){
     // sinon si i est un multiple de 7

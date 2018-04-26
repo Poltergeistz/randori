04bfb193901613726c6605d1b604d8b7794ad730
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-26 11:10:33 +0200

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+for (i=0,i<=100,i++){
+}
+  }if{ (i%35 == 0)
+  console.log("fizzbuz")
+  }else if (i%7 == 0){
+  console.log("buzz")
+  }else if (i%5 == 0){
+  console.log("fizz")
+  }else{
+  console.log(i)
+}
+}

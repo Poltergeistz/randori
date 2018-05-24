4c541c8acf03e554ab869275c282ab61d6561b04
david <david.journo.31@gmail.com>
2018-05-24 15:49:33 +0200

    randori update

--- randori.js
+++ randori.js
@@ -2,11 +2,25 @@
     Ecrivez une fonction minutesTillMidnight(heure), où heure est sous la forme "HH:MM".
     Cette fonction renvoie le nombre de minutes qui séparent heure de minuit.
 */
-var heure = new Date('May 24, 18 11:21'); // 'May 24, 18 11:21'
-var minuit = new Date('May 25, 18 24:00')
-var result = minuit.getHours()+ ":" +minuit.getMinutes() -  heure.getHours()+ ":" +heure.getMinutes();
 
-console.log(heure.getHours()+ ":" +heure.getMinutes());
-console.log(minuit.getHours()+ ":" +minuit.getMinutes());
 
-console.log(result);
+function minutesTillMidnight(heure){
+    let heure = new Date();
+    heure.setHours(17,0,0,0) // soit on met une valeur arbitraire soit celle de l'ordi
+    let minuit = new Date()
+    minuit.setHours(24,0,0,0) // defini l'heure, les minutes, les secondes et les milisecondes.
+    console.log(minuit.toString())
+    console.log(heure.toString())
+    let resultMilisecondes = minuit.getTime() - heure.getTime()
+    console.log(resultMilisecondes) // resultat en milisecondes
+    let arrondirMinutes = Math.ceil(resultMilisecondes/60000) // Math.ceil() retourne le plus petit entier supérieur ou égal au nombre donné.
+    console.log(arrondirMinutes)
+    return arrondirMinutes
+    }
+    
+    MinutestoMidnight()
+    
+    function convertMinutesToHours(){
+    
+    }
+    
